d1dbce4d5083f44199ad1080b78b1b806a74e18c
Peter Jackson <pjackson@students.mchenry.edu>
2018-01-23 16:52:54 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,26 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //a list of variables
+        byte myByte = -17;
+        short myShort = 1778;
+        int myInt = 9171978;
+        long myLong = 987654321;
+        float myFloat = 9.17f;
+        double myDouble = 9871709.1978d;
+        boolean myBoolean = false;
+        char myChar = 'p';
+
+        //display statements showing the type of variable and its assigned value
+        System.out.println("The value of my byte variable is:\t  " + myByte);
+        System.out.println("The value of my short variable is:\t  " + myShort);
+        System.out.println("The value of my int variable is:\t  " + myInt);
+        System.out.println("The value of my long variable is:\t  " + myLong);
+        System.out.println("The value of my float variable is:\t  " + myFloat);
+        System.out.println("The value of my double variable is:\t  " + myDouble);
+        System.out.println("The value of my boolean variable is:  " + myBoolean);
+        System.out.println("The value of my char variable is:\t  " + myChar);
+    }
+}
